a5af6324db1d64157a3077425af36648672edf72
hyperchessbot <hyperchessbot@gmail.com>
2021-01-13 13:04:16 +0000

    Simple request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("reqwest stream");
+    let body = reqwest::get("https://www.rust-lang.org")
+      .await?
+      .text()
+      .await?;
+
+    println!("body = {:?}", body);
 
     Ok(())
 }
